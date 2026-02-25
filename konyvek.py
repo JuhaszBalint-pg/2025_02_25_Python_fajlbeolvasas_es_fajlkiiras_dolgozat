11d4fb2163fba94d7fc965fc18607772d77d2f42
JuhaszBalint-pg <juhasz.balint@diak.szbi-pg.hu>
2026-02-25 08:58:53 +0100

    szunet

--- konyvek.py
+++ konyvek.py
@@ -13,10 +13,66 @@ Olvasd be a konyvek.txt adatait, majd oldd meg az alábbi feladatokat!
 A megoldott feladatokat a kiirt_adatok nevű mappában hozd létre statisztika.txt néven!
 """
 
+konyvek = []
+with open('beolvasando_adatok\konyvek.txt', 'r', encoding='utf-8') as forrasfajl:
+    next (forrasfajl)
 
-print("1. A beolvasott fájlban összesen ____ könyv szerepel.")
-print("2. A legtöbb oldalas könyv: ____")
-print("3. A legkevesebb oldalas könyv: ____")
-print("4. A legtöbb könyvet író szerző: ____")
-print("5. Az átlagos oldalszám: ____")
-print("***A legtöbb könyvet kiadó kiadó: ____")
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        cim = adatok[0]
+        szerzo = adatok[1]
+        oldalszam = int(adatok[2])
+        kiado = adatok[3]
+
+        konyv = {'cím': cim, 'szerző': szerzo, 'oldalszám': int(oldalszam), 'kiadó' : kiado}
+        konyvek.append(konyv)
+
+print(f'{konyvek=}')
+
+#1. feladat: Hány könyv szerepel a fájlban?
+konyvekszama = len(konyvek)
+
+print(f"1. A beolvasott fájlban összesen {konyvekszama} könyv szerepel.")
+
+#2. feladat: Melyik könyvnek van a legtöbb oldala?
+legtobb_oldalszam = 0
+legtobb_oldalu_konyv = str()
+for konyv in konyvek:
+    if konyv['oldalszám'] > legtobb_oldalszam:
+        legtobb_oldalszam = konyv['oldalszám']
+        legtobb_oldalu_konyv = konyv['cím']
+
+print(f"2. A legtöbb oldalas könyv: {legtobb_oldalu_konyv}")
+
+#3. feladat: Melyik könyvnek van a legkevesebb oldala?
+
+legkevesebb_oldalszam = 1000000
+legkevesebb_oldalu_konyv = str()
+for konyv in konyvek:
+    if konyv['oldalszám'] < legkevesebb_oldalszam:
+        legkevesebb_oldalszam = konyv['oldalszám']
+        legkevesebb_oldalu_konyv = konyv['cím']
+
+print(f"3. A legkevesesbb oldalas könyv: {legkevesebb_oldalu_konyv}")
+
+#4. feladat: Melyik szerző írt a legtöbb könyvet?
+for konyv in konyvek:
+    
+
+
+
+print(f"4. A legtöbb könyvet író szerző:")
+
+#5. feladat: Átlagosan hány oldalasak a könyvek?
+osszoldalszam = 0
+for konyv in konyvek:
+    osszoldalszam += konyv['oldalszám']
+    atlagoldalszam = osszoldalszam/len(konyvek)
+print(f"5. Az átlagos oldalszám: {oldalszam}")
+
+with open('kiirt_adatok\statisztika.txt', 'w', encoding='utf-8') as celfajl:
+    celfajl.write(f'A beolvasott fájlban összesen {konyvekszama} könyv szerepel.\n')
+    celfajl.write(f'A legtöbb oldalas könyv: {legtobb_oldalu_konyv}\n')
+    celfajl.write(f'A legkevesesbb oldalas könyv: {legkevesebb_oldalu_konyv}\n')
+    celfajl.write(f'\n')
+    celfajl.write(f'Az átlagos oldalszám: {oldalszam}')
